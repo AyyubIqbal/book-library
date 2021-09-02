3b8b12a720b631f161f51d006f4f96ee93a5495a
ayyubiqbal <abuayubiqbal@gmail.com>
2021-09-02 22:03:47 +0600

    no result error text added

--- books.js
+++ books.js
@@ -13,23 +13,34 @@ const loadData = () => {
 const displayData = (books) => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
-    // console.log(books);
+    console.log(books);
 
-    books.forEach(book => {
+    if (books.length === 0) {
         const div = document.createElement('div')
         div.classList.add('col');
-        const img = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
         div.innerHTML = `
-                <div  class="card h-100">
-                    
-                    <div class="card-body">
-                    <img src="${img}" class="card-img-top" alt="...">
-                        <h5 class="card-title">Book Name: ${book.title}</h5>
-                        <p class="card-text">Author: ${book.author_name}</p>
-                        <p class="card-text">first publish year: ${book.first_publish_year}</p>
-                        <p class="card-text">publisher: ${book.publisher}</p>
-                    </div>
-                 </div>`;
-        searchResult.appendChild(div);
-    })
+        <h2 class="card-title">No result found</h2>  
+        `;
+        searchResult.appendChild(div)
+    }
+
+    else {
+        books.forEach(book => {
+            const div = document.createElement('div')
+            div.classList.add('col');
+            const img = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+            div.innerHTML = `
+                    <div  class="card h-100">
+                        
+                        <div class="card-body">
+                        <img src="${img}" class="card-img-top" alt="...">
+                            <h5 class="card-title">Book Name: ${book.title}</h5>
+                            <p class="card-text">Author: ${book.author_name}</p>
+                            <p class="card-text">first publish year: ${book.first_publish_year}</p>
+                            <p class="card-text">publisher: ${book.publisher}</p>
+                        </div>
+                     </div>`;
+            searchResult.appendChild(div);
+        })
+    }
 }
